105717207b2206ae38cee9101df650ed0fe7f056
Nik Bailey <bail0301@morris.umn.edu>
2022-11-18 14:10:06 -0600

    Completed implementation of task 2
    Just need to convert from char to string for the letter

--- lab11.java
+++ lab11.java
@@ -1,44 +1,64 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class lab11 {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws NumberFormatException, IOException {
         // Initializing variables
         String inputFileName;
         int size = 0;
         String generic;
 
         // Putting in a scanner to read a file name
-        Scanner s = new Scanner(System.in);
+        Scanner scant = new Scanner(System.in);
         System.out.println("nom de fichier d'input: ");
-        inputFileName = s.nextLine();
+        inputFileName = scant.nextLine();
 
         // Reading the file, both read the same file
-        BufferedReader br = new BufferedReader(new FileReader(inputFileName));
-        BufferedReader stupid = new BufferedReader(new FileReader(inputFileName));
+        BufferedReader br1 = new BufferedReader(new FileReader(inputFileName));
+        BufferedReader br2 = new BufferedReader(new FileReader(inputFileName));
 
         // Getting the number of lines in the file
         // # of lines = # of letters
-        while((generic = br.readLine()) != null) {
+        while((generic = br1.readLine()) != null) {
             size++;
         }
 
-        PriorityQueue<Node> betterName = new PriorityQueue<Node>(size, new Comparato());
+        PriorityQueue<Node> priorQueueue = new PriorityQueue<Node>(size, new Comparato());
 
         // Creating a node for each letter
-        while((generic = stupid.readLine()) != null) {
+        while((generic = br2.readLine()) != null) {
             String letter = generic.substring(0, 1);
             String frequency = generic.substring(2);
             int freq = Integer.parseInt(frequency);
-            betterName.add(new Node(letter, freq));       
+            priorQueueue.add(new Node(letter, freq));       
         }
 
-        for(int i = 0; i < size; i++) {
-            Node noded = new Node();
+        Node root = null;
 
-            noded.
+        while (priorQueueue.size() > 1) {
+            Node x = priorQueueue.peek();
+            priorQueueue.poll();
+
+            Node y = priorQueueue.peek();
+            priorQueueue.poll();
+
+            Node f = new Node("-", x.data + y.data);
+            f.left = x;
+            f.right = y;
+            root = f;
+            priorQueueue.add(f);
         }
+    }
+
+    public static void printCode(Node root, String stringe) {
+        if(root.left == null && root.right == null && Character.isLetter(root.s)) {
+            System.out.println(root.s + ":" + stringe);
+        }
+
+        printCode(root.left, stringe + "0");
+        printCode(root.right, stringe + "1");
     }
 }
