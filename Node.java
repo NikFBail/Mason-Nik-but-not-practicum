01a7bb979daab06e67a4b637dfb2b4a67bf62f52
Nik Bailey <bail0301@morris.umn.edu>
2022-12-05 08:41:05 -0600

    Changes didn't work
    Leaving it as it is, and submitting

--- Node.java
+++ Node.java
@@ -5,16 +5,16 @@ public class Node{
     public Node rightChild;
     public Node parent;
 
-    public double letterFrequency;
+    public double frequency;
     public char letter;
     public String binary = "";
 
     public Node(){
-        letterFrequency = 100;
+        frequency = 100;
     }
 
     public Node(char character, double freq){
-        this.letterFrequency = freq;
+        this.frequency = freq;
         this.letter = character;
     }
 
@@ -24,7 +24,7 @@ public class Node{
     }
 
     public Node(Node left, Node right){
-        this.letterFrequency = left.letterFrequency + right.letterFrequency;
+        this.frequency = left.frequency + right.frequency;
         this.leftChild = left;
         this.rightChild = right;
         left.parent = this;
@@ -32,38 +32,38 @@ public class Node{
     }
 
     public String toString(){
-        return this.letter + " " + this.letterFrequency;
+        return this.letter + " " + this.frequency;
     }
 
     /**
      * rootToString is a toString method to turn a given node tree into a string
      * @param node Takes a root node of a node tree
-     * @return System outs using a postOrder traversal method outputting "letterFrequency||binary"
+     * @return System outs using a postOrder traversal method outputting "frequency||binary"
      * So that node's frequency next to its binary representation
      */
     public static String rootToString(Node node){
         if(node == null) return "";
-        return rootToString(node.leftChild) + rootToString(node.rightChild) + node.letterFrequency + "||" + node.binary + "    ";
+        return rootToString(node.leftChild) + rootToString(node.rightChild) + node.frequency + "||" + node.binary + "    ";
     }
 
     public int compareTo(Node node){
-        if(node.letterFrequency > this.letterFrequency) return -1;
-        if(node.letterFrequency < this.letterFrequency) return 1;
+        if(node.frequency > this.frequency) return -1;
+        if(node.frequency < this.frequency) return 1;
         return 0;
     }
 
 }
 
-class compareFrequency implements Comparator<Node>{
+class ComparatoFreq implements Comparator<Node>{
     public int compare(Node n1, Node n2){
-        if(n2.letterFrequency > n1.letterFrequency) return -1;
-        if(n2.letterFrequency < n1.letterFrequency) return 1;
+        if(n2.frequency > n1.frequency) return -1;
+        if(n2.frequency < n1.frequency) return 1;
         return 0;
     }
 
 }
 
-class compareBinary implements Comparator<Node>{
+class ComparatoBin implements Comparator<Node>{
     public int compare(Node n1, Node n2){
         if(n2.binary.length() > n1.binary.length()) return -1;
         if(n2.binary.length() < n1.binary.length()) return 1;
